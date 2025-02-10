63fab7d0ead228474e98c3d24a2ccd0c556ac84f
HelderMucondo33 <heldermucondo@gmail.com>
2025-02-10 17:01:40 +0200

    finanlizando a filtragem por ordenacao

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
     <div className="app">
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
@@ -73,6 +73,11 @@ function App() {
           )
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text) //a>b
+              : b.text.localeCompare(a.text)//b<a
           )
           .map((todo) => (
             <Todo
